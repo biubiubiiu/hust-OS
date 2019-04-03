ca182434a978f7c9aa159cef228f6581427de963
Raymond Wong <33112761+biubiubiiu@users.noreply.github.com>
2019-04-03 23:03:04 +0800

    Create main.cpp

---
+++ 4. 大作业: System monitor/QT version/main.cpp
@@ -0,0 +1,13 @@
+#include <QApplication>
+#include "sysmonitor.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    sysMonitor sm;
+    sm.show();
+    sm.getProcessInfo();
+
+    return a.exec();
+}
