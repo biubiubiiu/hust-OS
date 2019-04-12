53036978972444ee8588853680131c681e63b1aa
Raymond Wong <33112761+biubiubiiu@users.noreply.github.com>
2019-04-13 01:54:20 +0800

    Update chart.h

--- 4. 大作业: System monitor/QT version/chart.h
+++ 4. 大作业: System monitor/QT version/chart.h
@@ -5,8 +5,8 @@
 #include <QtCore/QTimer>
 
 QT_CHARTS_BEGIN_NAMESPACE
-class QSplineSeries;
 class QValueAxis;
+class QLineSeries;
 QT_CHARTS_END_NAMESPACE
 
 QT_CHARTS_USE_NAMESPACE
@@ -16,21 +16,18 @@ class Chart: public QChart
     Q_OBJECT
 public:
     Chart(QGraphicsItem *parent = 0, Qt::WindowFlags wFlags = 0);
-    void setFunc(double (*f)(void));
+    void refresh();
+    void append(double ratio);
 virtual ~Chart();
-
-public slots:
-    void handleTimeout();
 
 private:
     QTimer m_timer;
-    QSplineSeries *m_series;
+    QLineSeries *m_series;
     QStringList m_titles;
 //    QValueAxis *m_axisX;
 //    QValueAxis *m_axisY;
     QValueAxis *m_axis;
     QList<double> data;
-    double (*newData)();
 };
 
 #endif // CHART_H
