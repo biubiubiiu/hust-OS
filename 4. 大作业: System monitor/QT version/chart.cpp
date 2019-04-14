102b0b746fcfe6572d1aa54f7bcd65fe1009a18d
Raymond Wong <33112761+biubiubiiu@users.noreply.github.com>
2019-04-15 00:51:29 +0800

    Update chart.cpp

--- 4. 大作业: System monitor/QT version/chart.cpp
+++ 4. 大作业: System monitor/QT version/chart.cpp
@@ -20,8 +20,10 @@ Chart::Chart(QGraphicsItem *parent, Qt::WindowFlags wFlags):
     createDefaultAxes();
     setAxisX(m_axis, m_series);
     m_axis->setTickCount(6);
-    axisX()->setRange(60, 0);
+    axisX()->setRange(0, 60);
     axisY()->setRange(0.0, 100.0);
+    axisX()->setLabelsVisible(false);
+    axisY()->setLabelsVisible(false);
 
     // 以下为Chart类的自定义设置
     setAnimationOptions(QChart::NoAnimation);   // 默认关闭动画效果
@@ -35,22 +37,20 @@ Chart::~Chart()
 
 void Chart::append(double ratio)
 {
-    // 先判断一下数据有效性，小心驶得万年船
+    // 先判断一下数据有效性，免得出事
     if(ratio >= 0 && ratio <= 100)
         data << ratio;
 }
 
 void Chart::refresh()
 {
-    // 图表中最多容纳60个数据
+    // 维持数表中不超过60个数据
     while(data.size() > 60)
         data.removeFirst();
-    if(isVisible())
-    {
-        m_series->clear();
-        int size = data.size();
-        for(int i = size - 1; i >= 0; --i){
-            m_series->append(60 - (size - 1 - i), data.at(i));  // 目的是为了让图像从右向左移动
-        }
+    // 更新坐标
+    m_series->clear();
+    int size = data.size();
+    for(int i = size - 1; i >= 0; --i){
+        m_series->append(60 - (size - 1 - i), data.at(i));  // 目的是为了让图像从右向左移动
     }
 }
