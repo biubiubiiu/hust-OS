e1790762a47f3891542e8d6d77c1b38db3170834
Raymond Wong <33112761+biubiubiiu@users.noreply.github.com>
2019-04-16 01:12:37 +0800

    Update utils.h

--- 4. 大作业: System monitor/QT version/utils.h
+++ 4. 大作业: System monitor/QT version/utils.h
@@ -4,8 +4,12 @@
 #include <QString>
 #include <QFont>
 #include <QFontMetrics>
+#include <windows.h>
 
 QString getElidedText(QFont font, QString str, int maxWidth);
 QString transformTime(unsigned long tick);
+BOOL setPrivilege(HANDLE hToken, LPCTSTR lpszPrivilege, BOOL bEnablePrivilege);
+HANDLE getProcessHandle(DWORD dwProcessId);
+bool EnableDebugPrivilege();
 
 #endif // UTILS_H
