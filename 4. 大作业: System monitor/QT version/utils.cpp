93535585239b8236c591b61763c7047ca89b55d3
Raymond Wong <33112761+biubiubiiu@users.noreply.github.com>
2019-04-16 01:12:14 +0800

    Update utils.cpp

--- 4. 大作业: System monitor/QT version/utils.cpp
+++ 4. 大作业: System monitor/QT version/utils.cpp
@@ -46,3 +46,84 @@ QString transformTime(unsigned long tick)
                                         .arg(seconds, 2, 10, QChar('0'));
     return ret;
 }
+
+BOOL SetPrivilege(
+                  HANDLE hToken,          // access token handle
+                  LPCTSTR lpszPrivilege,  // name of privilege to enable/disable
+                  BOOL bEnablePrivilege   // to enable or disable privilege
+                  )
+{
+    TOKEN_PRIVILEGES tp;
+    LUID luid;
+    if ( !LookupPrivilegeValue(
+        NULL,            // lookup privilege on local system
+        lpszPrivilege,   // privilege to lookup
+        &luid ) )        // receives LUID of privilege
+    {
+        printf("LookupPrivilegeValue error: %u/n", GetLastError() );
+        return FALSE;
+    }
+    tp.PrivilegeCount = 1;
+    tp.Privileges[0].Luid = luid;
+    if ( bEnablePrivilege )
+        tp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+    else
+        tp.Privileges[0].Attributes = 0;
+    // Enable the privilege or disable all privileges.
+    if ( !AdjustTokenPrivileges(
+        hToken,
+        FALSE,
+        &tp,
+        sizeof(TOKEN_PRIVILEGES),
+        (PTOKEN_PRIVILEGES) NULL,
+        (PDWORD) NULL) )
+    {
+        printf("AdjustTokenPrivileges error: %u/n", GetLastError() );
+        return FALSE;
+    }
+    if (GetLastError() == ERROR_NOT_ALL_ASSIGNED)
+    {
+        printf("The token does not have the specified privilege. /n");
+        return FALSE;
+    }
+    return TRUE;
+}
+
+HANDLE getProcessHandle(DWORD dwProcessId)
+{
+    HANDLE hToken;
+    bool flag = OpenProcessToken( GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES, &hToken );
+    if( !flag )
+    {
+        DWORD err = GetLastError();
+        printf( "OpenProcessToken error:%d", err );
+    }
+    SetPrivilege( hToken, SE_DEBUG_NAME, true );
+    CloseHandle(hToken);
+    return OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, FALSE, dwProcessId);
+}
+
+bool EnableDebugPrivilege()
+{
+    HANDLE hToken;
+    LUID sedebugnameValue;
+    TOKEN_PRIVILEGES tkp;
+    if (!OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &hToken))
+    {
+        return FALSE;
+    }
+    if (!LookupPrivilegeValue(NULL, SE_DEBUG_NAME, &sedebugnameValue))
+    {
+        CloseHandle(hToken);
+        return false;
+    }
+    tkp.PrivilegeCount = 1;
+    tkp.Privileges[0].Luid = sedebugnameValue;
+    tkp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+    if (!AdjustTokenPrivileges(hToken, FALSE, &tkp, sizeof(tkp), NULL, NULL))
+    {
+        CloseHandle(hToken);
+        return false;
+    }
+    return true;
+}
